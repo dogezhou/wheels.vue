4c92aeb473308b8294fa385f76d839409b3de6a7
dogezhou <doge.zhou@outlook.com>
2019-02-08 15:03:05 +0800

    按钮icon

--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,24 @@
 <template>
-  <button class="z-button">按钮</button>
+  <button
+    class="z-button"
+    :class="{[`icon-${iconPosition}`] : true}"
+  >
+    <svg
+      v-if="icon"
+      class="icon"
+    >
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot>
+        <slot>
+    </div>
+  </button>
 </template>
 
 <script>
 export default {
+  props: ['icon', 'iconPosition']
 }
 </script>
 
@@ -15,16 +30,29 @@ export default {
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+  vertical-align: top;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
   &:hover {
     border-color: var(--border-color-hover);
   }
-
   &:active {
     background-color: var(--button-active-bg);
   }
-
   &:focus {
     outline: none;
+  }
+
+  .icon {
+    margin-right: 0.1em;
+  }
+  &.icon-right {
+    > .icon {
+      order: 2;
+      margin-left: 0.1em;
+      margin-right: 0;
+    }
   }
 }
 </style>
