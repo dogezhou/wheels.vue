06f5d4de5d1c7600af2a39185f94650b791d1281
dogezhou <doge.zhou@outlook.com>
2019-02-28 21:54:34 +0800

    检测buttton-group 子元素是不是button

--- src/button-group.vue
+++ src/button-group.vue
@@ -6,13 +6,21 @@
 
 <script>
 export default {
-
+  mounted() {
+    for (const node of this.$el.children) {
+      const name = node.nodeName.toLowerCase()
+      if (name !== 'button') {
+        console.warn(`z-button-group 的子元素应该全是 button, 但是你写的是 ${name}`)
+      }
+    }
+  }
 }
 </script>
 
 <style lang="scss">
 .z-button-group {
   display: inline-flex;
+  vertical-align: top;
   > .z-button {
     border-radius: 0;
     margin-left: -1px;
