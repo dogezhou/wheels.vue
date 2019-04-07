6597f5a2ca3e5512f13ccf6e246a5ea64a3983db
dogezhou <doge.zhou@outlook.com>
2019-04-07 15:26:39 +0800

    基本完成栅格布局组件

---
+++ src/col.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    class="col"
+    :class="[`col-${span}`]"
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ZCol',
+  props: {
+    span: {
+      type: [Number, String],
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.col {
+  height: 100px;
+  background: grey;
+  width: 50%;
+  border: 1px solid black;
+}
+
+$class: col-;
+@for $n from 1 through 24 {
+  .col.#{$class}#{$n} {
+    width: ($n / 24) * 100%;
+  }
+}
+</style>
