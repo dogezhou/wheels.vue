890185d279c1d9883d6e1d5f022ca1b5386ee9e7
dogezhou <doge.zhou@outlook.com>
2019-09-13 00:33:23 +0800

    实现 toast 只能有一个

--- src/plugin.js
+++ src/plugin.js
@@ -1,14 +1,25 @@
 import Toast from './toast'
+
+function createToast ({Vue, message, propsData}) {
+  let Constructor = Vue.extend(Toast)
+  let toast = new Constructor({
+    propsData
+  })
+  toast.$slots.default = [message]
+  toast.$mount()  // 到内存中
+  document.body.appendChild(toast.$el) // 并挂载到 document 上
+  return toast
+}
+
+let currentToast
+
 export default {
   install(Vue, options) {
     Vue.prototype.$toast = function (message, toastOptions) {
-      let Constructor = Vue.extend(Toast)
-      let toast = new Constructor({
-        propsData: toastOptions
-      })
-      toast.$slots.default = [message]
-      toast.$mount()  // 到内存中
-      document.body.appendChild(toast.$el) // 并挂载到 document 上
+      if (currentToast) {
+        currentToast.close()
+      }
+      currentToast = createToast({Vue, message, propsData: toastOptions})
     }
   }
 }
