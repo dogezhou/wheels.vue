c9e64f72d037e2942e76a9032261075b2c6eb23c
dogezhou <doge.zhou@outlook.com>
2019-09-24 22:41:28 +0800

    触发 update:selected 事件时添加 tab-item vm

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="tabs-head">
     <slot></slot>
+    <div class="line" ref="line"></div>
     <div class="actions-wrapper">
       <slot name="actions"></slot>
     </div>
@@ -11,15 +12,26 @@
     name: 'ZTabsHead',
     inject: ['eventBus'],
     created () {
+      this.eventBus.$on('update:selected', (item, vm) => {
+        console.log('选中的元素', vm.$el)
+      })
     }
   }
 </script>
 <style scoped lang="scss">
   $tab-height: 40px;
+  $blue: blue;
   .tabs-head {
     display: flex;
     height: $tab-height;
     justify-content: flex-start;
+    position: relative;
+    > .line {
+      position: absolute;
+      bottom: 0;
+      border-bottom: 1px solid $blue;
+      width: 100px;
+    }
     > .actions-wrapper {
       margin-left: auto;
     }
--- src/tabs.vue
+++ src/tabs.vue
@@ -31,7 +31,15 @@
       }
     },
     mounted () {
-      this.eventBus.$emit('update:selected', this.selected)
+      this.$children.forEach((vm) => {
+        if (vm.$options.name === 'ZTabsHead') {
+          vm.$children.forEach((childVm) => {
+            if (childVm.$options.name === 'ZTabsItem' && childVm.name === this.selected) {
+              this.eventBus.$emit('update:selected', this.selected, childVm)
+            }
+          })
+        }
+      })
     }
   }
 </script>
