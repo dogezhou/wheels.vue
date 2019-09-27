62a75a3b6cb5da26bf6d3ac8ee899d63a168c808
dogezhou <doge.zhou@outlook.com>
2019-09-27 17:07:06 +0800

    popover position 测试用例

---
+++ test/popover.test.js
@@ -0,0 +1,32 @@
+const expect = chai.expect;
+import Vue from 'vue'
+import Popover from '../src/popover'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('popover 组件', () => {
+  it('存在', () => {
+    expect(Popover).to.be.exist
+  })
+
+  it('设置 position', (done) => {
+    Vue.component('z-popover', Popover)
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    div.innerHTML = `
+      <z-popover position="bottom" ref="popover">
+        <template slot="content">弹出能容</template>
+        <button>按钮</button>
+      </z-popover>
+    `
+    const vm = new Vue({
+      el: div
+    })
+    vm.$el.querySelector('button').click()
+    vm.$nextTick(() => {
+      expect(vm.$refs.popover.$refs.contentWrapper.classList.contains('position-bottom')).to.be.true
+      done()
+    })
+  })
+})
