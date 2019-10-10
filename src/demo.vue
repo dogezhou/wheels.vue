cd82eca989438d3483882dafb75025373e10e483
dogezhou <doge.zhou@outlook.com>
2019-10-10 22:40:23 +0800

    使用 vue-cli 代替 parcel

---
+++ src/demo.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <z-cascader
+      :source="source"
+      popover-height="200px"
+    ></z-cascader>
+  </div>
+</template>
+
+<script>
+import Cascader from './cascader'
+
+export default {
+  name: 'demo',
+  components: {
+    'z-cascader': Cascader
+  },
+  data() {
+    return {
+      source: [
+        {
+          name: '浙江',
+          children: [
+            {
+              name: '杭州',
+              children: [
+                {
+                  name: '西湖'
+                }
+              ]
+            },
+          ],
+        },
+        {
+          name: '江苏',
+          children: [
+            {
+              name: '南京',
+              children: [
+                {
+                  name: '江宁区'
+                }
+              ]
+            },
+          ],
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+  }
+</style>
