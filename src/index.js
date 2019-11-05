877479f28b4146b28ffaa66728bbf11d94f4d0dd
dogezhou <doge.zhou@outlook.com>
2019-11-05 18:15:23 +0800

    publish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 // 打包入口文件 build lib entry
 
-export { default as Button } from './button'
+export {default as Button} from './button'
+export {default as ButtonGroup} from './button-group'
+
+export {default as Cascader} from './cascader'
+export {default as CascaderItems} from './cascader/cascader-items'
+
+export {default as CollapseItem} from './collapse-item'
+export {default as Collapse} from './collapse'
+
+export {default as Col} from './col'
+export {default as Row} from './row'
+export {default as Content} from './content'
+export {default as Footer} from './footer'
+export {default as Header} from './header'
+export {default as Layout} from './layout'
+export {default as Sider} from './sider'
+
+export {default as Tabs} from './tabs'
+export {default as TabsBody} from './tabs-body'
+export {default as TabsHead} from './tabs-head'
+export {default as TabsItem} from './tabs-item'
+export {default as TabsPane} from './tabs-pane'
+
+export {default as ClickOutside} from './click-outside'
+export {default as Icon} from './icon'
+export {default as Input} from './input'
+export {default as Plugin} from './plugin'
+export {default as Popover} from './popover'
+export {default as Toast} from './toast'
